658a551b7d086b034cc6f2bcfbf1034abb5e18e5
atmatto <mattoover@gmail.com>
2022-08-25 13:05:03 +0200

    Show warnings

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -O3 -static -std=c++17 -o test $@ # https://oi.edu.pl/l/28oi_ustalenia_techniczne/
+g++ -Wall -Wextra -O3 -static -std=c++17 -o test $@ # https://oi.edu.pl/l/28oi_ustalenia_techniczne/
 # g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE -o test $@ # UVa
